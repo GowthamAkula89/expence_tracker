1733c1259c93b85259eebe7b83d77476fd9d4adc
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-03-26 15:06:36 +0530

    updated recent transactions

--- src/App.js
+++ src/App.js
@@ -5,16 +5,11 @@ import ExpensesList from './Components/ExpensesList';
 function App() {
   const [expensesList, setExpensesList] = useState([
     { title: 'Treaking', price: 45, category: 'Entertainment', date: '25/08/2023' },
-    { title: '12th fail', price: 154, category: 'Movie', date: '25/02/2024' },
+    { title: '12th fail', price: 154, category: 'Entertainment', date: '25/02/2024' },
     { title: 'Biryani', price: 254, category: 'Food', date: '26/02/2024' }
   ]);
   const[totalExpenses, setTotalExpenses] = useState(0);
-  // Function to compare dates for sorting
-  const compareDates = (a, b) => {
-    const dateA = new Date(a.date.split("/").reverse().join("-"));
-    const dateB = new Date(b.date.split("/").reverse().join("-"));
-    return dateA - dateB;
-  };
+  
   useEffect(()=>{
     const totalExpensesValue = expensesList.reduce((total, expense) => total + expense.price, 0);
     setTotalExpenses(totalExpensesValue);
@@ -38,17 +33,8 @@ function App() {
               setExpensesList={setExpensesList}/>
         </div>
       </div>
-      <div>
-        <div className="expense-list-heading">Recent Transactions</div>
-          <div className='expenses-list'>
-            {expensesList.sort(compareDates).map((expense, index) => (
-                        <div key={index}>
-                            <ExpensesList expense={expense}/>
-                        </div>
-            ))}
-          </div>
-          
-      </div>
+      <ExpensesList expensesList={expensesList} setExpensesList={setExpensesList}/>
+      
     </div>
   );
 }
