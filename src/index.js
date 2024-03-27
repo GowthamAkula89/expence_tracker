3d6c7ca5939e168e3961958b644350ca873f0be7
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-03-28 01:28:40 +0530

    final expense tracker

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { SnackbarProvider } from 'notistack';
 import reportWebVitals from './reportWebVitals';
-import Modal from 'react-modal';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-Modal.setAppElement('#root');
+
 root.render(
   <React.StrictMode>
-    <App />
-    
+    <SnackbarProvider maxSnack={3}>
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
